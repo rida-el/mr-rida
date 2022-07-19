4ff1a694e38e3dcc36de4b9881366cf9b8ec8421
rida-el <rida.el99@gmail.com>
2022-07-19 16:29:52 +0100

    set up the project

---
+++ src/components/navBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Navbar() {
+    return (
+        <div className="App">
+        otawssss
+        </div>
+      );
+}
+
+export default Navbar;
